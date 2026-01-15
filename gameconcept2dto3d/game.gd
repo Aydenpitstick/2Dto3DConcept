e1e113a4828e99686df1b83f39c2f7283123dd1c
Aydenpitstick <108236392+Aydenpitstick@users.noreply.github.com>
2026-01-15 13:13:47 -0500

    Separated Character movement
    I was able to separate the 2d and 3d movement so they move individually when switching camera. I also made the 2d character only able to move left and right and I added a background and the 2d wizard to the 2d character.

--- gameconcept2dto3d/game.gd
+++ gameconcept2dto3d/game.gd
@@ -3,13 +3,19 @@ extends Node
 @onready var TwoDCam = $"2D/Cam2D"
 @onready var ThreeDCam = $"3D/Cam3D"
 
+signal TwoDActive
+signal ThreeDActive
 func _ready() -> void:
-	
-	
 	TwoDCam.current = true
+	TwoDActive.emit()
 	ThreeDCam.current = false
 
 func _process(delta: float) -> void:
+	if TwoDCam.current == true:
+		TwoDActive.emit()
+	if ThreeDCam.current == true:
+		ThreeDActive.emit()
+	
 	if Input.is_action_just_pressed("switch"):
 		if TwoDCam.current == true:
 			TwoDCam.current = false
