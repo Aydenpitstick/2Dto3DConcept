14b27d78c6a597770bdb7c4b49b1e2da6a4b43ac
Aydenpitstick <108236392+Aydenpitstick@users.noreply.github.com>
2026-01-28 11:51:32 -0500

    Added music to prototype

--- gameconcept2dto3d/game.gd
+++ gameconcept2dto3d/game.gd
@@ -2,12 +2,18 @@ extends Node
 
 @onready var TwoDCam = $"2D/Cam2D"
 @onready var ThreeDCam = $"3D/Cam3D"
+@onready var TwoDMusic = $"BG2D"
+@onready var ThreeDMusic = $"BG3D"
+
 
 signal TwoDActive
 signal ThreeDActive
 func _ready() -> void:
 	TwoDCam.current = true
 	TwoDActive.emit()
+	TwoDMusic.play()
+	ThreeDMusic.play()
+	ThreeDMusic.volume_db = -80
 	ThreeDCam.current = false
 
 func _process(delta: float) -> void:
@@ -20,6 +26,10 @@ func _process(delta: float) -> void:
 		if TwoDCam.current == true:
 			TwoDCam.current = false
 			ThreeDCam.current = true
+			ThreeDMusic.volume_db = 0
+			TwoDMusic.volume_db = -80
 		elif ThreeDCam.current == true:
 			ThreeDCam.current = false
 			TwoDCam.current = true
+			TwoDMusic.volume_db = 0
+			ThreeDMusic.volume_db = -80
